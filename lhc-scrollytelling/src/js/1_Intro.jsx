f471f5b9227d1c452b3cece1996368ea801dd414
aliciachickenwings <aachalshresth@gmail.com>
2025-05-30 15:28:31 +0200

    WIP ParticleTube path animation

---
+++ lhc-scrollytelling/src/js/1_Intro.jsx
@@ -0,0 +1,64 @@
+import "../css/Intro.css";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import MainParticle from "./components/mainParticle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function Intro() {
+  const subtitleRef = useRef(null);
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const subtitleEl = subtitleRef.current;
+    const imgEl = imgRef.current;
+
+    gsap.to(imgEl, {
+      scale: 0.8,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: subtitleEl,
+        start: "bottom top",
+        end: "bottom top+=100",
+        scrub: true,
+      },
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
+  return (
+    <>
+      <div>
+        <div>
+          <h1 className="big-title main-title">MAKING THE INVISIBLE VISIBLE</h1>
+        </div>
+        <p>
+          How new the Higgs Boson (and other particles) are discovered at Cern
+        </p>
+      </div>
+      <div className="pt-2">
+        <h1 ref={subtitleRef} className="big-title sub-title">
+          Some particles, like the Higgs boson, do not exist naturally in our
+          everyday world.
+        </h1>
+        {/*         <div className="img-main">
+          <img
+            ref={imgRef}
+            className="floating-img"
+            src="/assets/particles/pink/O_MAIN.png"
+          />
+        </div> */}
+        <MainParticle ref={imgRef}></MainParticle>
+      </div>
+      <img src="/assets/particles/pink/O_2.png" alt="Particle 2" />
+      <img src="/assets/particles/pink/O_2.png" alt="Particle 2" />
+      <img src="/assets/particles/pink/O_2.png" alt="Particle 2" />
+    </>
+  );
+}
+
+export default Intro;
--- lhc-scrollytelling/src/js/main.jsx
+++ lhc-scrollytelling/src/js/main.jsx
@@ -1,10 +1,12 @@
+// main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../css/index.css";
-import Intro from "./Intro.jsx";
+import Intro from "./1_Intro";
+import ParticlesTube from "./2_Particle_tube";
+import "../index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Intro />
+    <ParticlesTube />
   </StrictMode>
 );
